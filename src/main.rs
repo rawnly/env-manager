5b465c71f75ed2f88c15be5537eba3b27001fbc2
rawnly <rawnly@users.noreply.github.com>
2023-09-29 12:23:15 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,85 @@
+use std::collections::HashMap;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Subcommand, Debug)]
+enum SubCommand {
+    /// Print all .env variables
+    List,
+
+    /// Set environment
+    Set { key: String, value: String },
+
+    // don't use flags
+    /// Get environment
+    Get { key: String },
+}
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[clap(subcommand)]
+    cmd: SubCommand,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = Cli::parse();
+
+    let has_dotenv = tokio::fs::try_exists(".env").await?;
+
+    if !has_dotenv {
+        println!("No .env file found");
+        return Ok(());
+    }
+
+    match args.cmd {
+        SubCommand::List => {
+            let values = get_env().await?;
+
+            for (key, value) in values {
+                println!("{}={}", key, value);
+            }
+        }
+        SubCommand::Get { key } => {
+            let values = get_env().await?;
+
+            if let Some(value) = values.get(&key) {
+                println!("{}", value);
+            } else {
+                println!("No value found for key: {}", key);
+            }
+        }
+        SubCommand::Set { key, value } => {
+            let mut values = get_env().await?;
+
+            values.insert(key.clone(), value.clone());
+
+            let mut file_content = String::new();
+
+            for (key, value) in values {
+                file_content.push_str(&format!("{}={}\n", key, value));
+            }
+
+            tokio::fs::write(".env", file_content).await?;
+
+            println!("{}={}", key, value);
+        }
+    }
+
+    Ok(())
+}
+
+
+async fn get_env() -> anyhow::Result<HashMap<String, String>> {
+    // find .env file in the cwd
+    let file_content = tokio::fs::read_to_string(".env").await?;
+    let mut hashmap: HashMap<String, String> = HashMap::new();
+
+    file_content.split('\n').for_each(|row| {
+        if let Some((a, b)) = row.split_once('=') {
+            hashmap.insert(a.to_string(), b.to_string());
+        }
+    });
+
+    Ok(hashmap)
+}
